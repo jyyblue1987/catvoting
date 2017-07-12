14ae95c238549806d2347ad40e3ef223cd673466
jyyblue <jyyblue1987@outlook.com>
2017-07-12 09:49:14 +0800

    [fixed] show cat image with 4 column.

--- assets/admin/js/voting/voting.js
+++ assets/admin/js/voting/voting.js
@@ -7,11 +7,14 @@ app.controller('VotingController', function($scope, $rootScope, $http, $state, $
 
     $rootScope.page_title = 'Voting Page';
 
+    $scope.cat_list = [];
+
     // get cat image list
     function getCatImageList() {
         CatService.get(100)
             .then(function(response) {
-                console.log(response);
+                console.log(response);                
+                sortCatList(response.data.response.data.images.image);
             }).catch(function(response) {
 
             })
@@ -21,7 +24,24 @@ app.controller('VotingController', function($scope, $rootScope, $http, $state, $
 
     getCatImageList();
 
-    function parseXML() {
-        
+    function sortCatList(list) {
+        var col = 4;
+        var length = list.length;
+        var total_row_count = (length - 1) / col + 1;
+        var q = 0;
+        for(var i = 0; i < total_row_count; i++) {
+            var row = [];
+            for(var j = 0; j < col; j++ )
+            {
+                if( q >= length )
+                    break;
+
+                row.push(list[q]);
+                q++;
+            }
+
+            $scope.cat_list.push(row);
+        }
     }
+
 });
