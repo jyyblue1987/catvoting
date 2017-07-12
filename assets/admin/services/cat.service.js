e9abfd00677ea8cba734fc4d69208ff49a6f8f20
jyyblue <jyyblue1987@outlook.com>
2017-07-12 22:01:03 +0800

    [fixed] change sub id.

--- assets/admin/services/cat.service.js
+++ assets/admin/services/cat.service.js
@@ -6,7 +6,7 @@ app.factory('CatService',
 
         var API_URL = 'http://thecatapi.com/api';
         var api_key = 'MjAyOTE0';
-        var sub_id = '100';
+        var sub_id = '12222';
 
         service.get = function(page) {
             return $http.get(API_URL + '/images/get?format=xml&results_per_page=' + page + '&sub_id='+sub_id,
@@ -21,6 +21,7 @@ app.factory('CatService',
 
         service.vote = function(item) {
             var url = API_URL + '/images/vote?api_key=' + api_key + '&sub_id=' + sub_id + '&image_id=' + item.id + '&score=' + item.score;
+            console.log(url);
             return $http.get(url,
                 {
                     transformResponse: function (cnv) {
@@ -33,6 +34,7 @@ app.factory('CatService',
 
         service.getvotes = function(item) {
             var url = API_URL + '/images/getvotes?api_key=' + api_key + '&sub_id=' + sub_id;
+            console.log(url);
             return $http.get(url,
                 {
                     transformResponse: function (cnv) {
@@ -45,6 +47,7 @@ app.factory('CatService',
 
         service.favourite = function(item) {            
             var url = API_URL + '/images/favourite?api_key=' + api_key + '&sub_id=' + sub_id + '&image_id=' + item.id;
+            console.log(url);
             return $http.get(url,
                 {
                     transformResponse: function (cnv) {
@@ -57,6 +60,7 @@ app.factory('CatService',
 
         service.getfavourites = function(item) {
             var url = API_URL + '/images/getfavourites?api_key=' + api_key + '&sub_id=' + sub_id;
+            console.log(url);
             return $http.get(url,
                 {
                     transformResponse: function (cnv) {
